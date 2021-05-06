00c51fd1fbad4ebd4575997fcaae0671994a2627
jmart6784 <jmartinez6784@yahoo.com>
2021-05-05 21:07:39 -0700

    Added selection sort algorithm

--- algorithms.js
+++ algorithms.js
@@ -47,5 +47,35 @@ var primes = [
   97,
 ];
 
-var result = doSearch(primes, 73);
-console.log("Found prime at index " + result);
+// var result = doSearch(primes, 73);
+// console.log("Found prime at index " + result);
+
+var swap = function (array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function (array, startIndex) {
+  var minValue = array[startIndex];
+  var minIndex = startIndex;
+
+  for (var i = minIndex + 1; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minIndex = i;
+      minValue = array[i];
+    }
+  }
+  return minIndex;
+};
+
+var selectionSort = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    var smallIndex = indexOfMinimum(array, i);
+    swap(array, smallIndex, i);
+  }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array);
+console.log("Array after sorting:  " + array);
